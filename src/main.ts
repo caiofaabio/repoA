2bf84f7a16bdac88532f6302acdb740df11976ed
Caio Fabio <109986771+caiofaabio@users.noreply.github.com>
2024-04-03 19:31:03 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,7 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import {inspect} from 'util'
 
-/* eslint-disable  @typescript-eslint/no-explicit-any */
-function hasErrorStatus(error: any): error is {status: number} {
-  return typeof error.status === 'number'
-}
-
-function getErrorMessage(error: unknown) {
-  if (error instanceof Error) return error.message
-  return String(error)
-}
 
 async function run(): Promise<void> {
-  console.log('start run')
-  try {
-    const inputs = {
-      token: core.getInput('token'),
-      repository: core.getInput('repository'),
-      eventType: core.getInput('event-type'),
-      clientPayload: core.getInput('client-payload'),
-    }
-    core.debug(`Inputs: ${inspect(inputs)}`)
-
-    const [owner, repo] = inputs.repository.split('/')
-
-    const octokit = github.getOctokit(inputs.token)
-
-    console.log(inputs, 'inputs')
-    console.log(inputs.eventType, 'eventType')
-    console.log(inputs.clientPayload, 'clientPayload')
-    console.log(owner, 'owner')
-    console.log(repo, 'repo')
-    console.log(octokit, 'octokit')
-
-    await octokit.rest.repos.createDispatchEvent({
-      owner: owner,
-      repo: repo,
-      event_type: inputs.eventType,
-      client_payload: JSON.parse(inputs.clientPayload)
-    })
-  } catch (error) {
-    core.debug(inspect(error))
-    if (hasErrorStatus(error) && error.status == 404) {
-      core.setFailed(
-        'Repository not found, OR token has insufficient permissions.'
-      )
-    } else {
-      core.setFailed(getErrorMessage(error))
-    }
-  }
-  console.log('end run')
+ console.log('here')
 }
 
 run()
