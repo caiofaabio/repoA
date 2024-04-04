1078b5c4237b10fa03dae3b88468a0c9a45b95b9
Caio Fabio <109986771+caiofaabio@users.noreply.github.com>
2024-04-04 16:53:25 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import * as core from '@actions/core';
-// const core = require('@actions/core');
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {inspect} from 'util'
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-function hasErrorStatus(error){
+function hasErrorStatus(error: any): error is {status: number} {
   return typeof error.status === 'number'
 }
 
-function getErrorMessage(error) {
+function getErrorMessage(error: unknown) {
   if (error instanceof Error) return error.message
   return String(error)
 }
 
-async function run() {
+async function run(): Promise<void> {
   try {
     const inputs = {
       clientPayload: core.getInput('client-payload'),
